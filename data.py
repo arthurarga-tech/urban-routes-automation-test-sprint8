191404647dd57b7ffb3f3fc527711a8e985cbd9d
arthurarga-tech <arthur.aarga@gmail.com>
2025-05-13 13:52:06 -0300

    Tarefa 2, 3, 4 e 5

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-e52ea0b3-c878-433d-81aa-dd20b1e0b5ee.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
